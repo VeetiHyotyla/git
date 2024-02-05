98bc3aa8e06f080f363d8a89b3e9d1d18b6ff987
VeetiHyotyla <156584474+VeetiHyotyla@users.noreply.github.com>
2024-02-05 16:56:21 +0200

    Delete js directory

--- js/funtions.js
+++
@@ -1,6 +0,0 @@
-function NoppaEri() {
-    var eriNro = Math.floor(Math.random() * 6) + 1;
-    var kuva = document.getElementById("diceImage");
-    kuva.src = "img/" + eriNro + ".png";
-    kuva.alt = "Noppa " + eriNro;
-}
